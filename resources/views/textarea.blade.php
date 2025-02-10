44bc4d8d70316c81f1fc7f06dc9c370ea21c98f0
Tally Schmeits <tally@schmeits.com>
2025-02-10 09:14:53 +0100

    Updated the blade views to the latest Filament versions

--- resources/views/textarea.blade.php
+++ resources/views/textarea.blade.php
@@ -36,51 +36,50 @@
                 ->class(['fi-fo-textarea overflow-hidden'])
         "
     >
-        <textarea
-            @keyup="characterCount = $event.target.value.length"
-            @if ($shouldAutosize)
+        <div wire:ignore.self style="height: '{{ $initialHeight . 'rem' }}'">
+            <textarea
+                @keyup="characterCount = $event.target.value.length"
+                x-ignore
                 @if (FilamentView::hasSpaMode())
-                    ax-load="visible"
-            @else
-                ax-load
-            @endif
-            ax-load-src="{{ \Filament\Support\Facades\FilamentAsset::getAlpineComponentSrc('textarea', 'filament/forms') }}"
-            x-data="textareaFormComponent({ initialHeight: @js($initialHeight) })"
-            x-ignore
-            x-intersect.once="render()"
-            x-on:input="render()"
-            x-on:resize.window="render()"
-            wire:ignore.style.height
+                    {{-- format-ignore-start --}}ax-load="visible || event (ax-modal-opened)"{{-- format-ignore-end --}}
+                @else
+                    ax-load
+                @endif
+                ax-load-src="{{ \Filament\Support\Facades\FilamentAsset::getAlpineComponentSrc('textarea', 'filament/forms') }}"
+                x-data="textareaFormComponent({
+                            initialHeight: @js($initialHeight),
+                            shouldAutosize: @js($shouldAutosize),
+                            state: $wire.$entangle('{{ $statePath }}'),
+                        })"
+                @if ($shouldAutosize)
+                    x-intersect.once="resize()"
+                    x-on:resize.window="resize()"
+                @endif
+                x-model="state"
                 {{ $getExtraAlpineAttributeBag() }}
-            @endif
-            {{
-                $getExtraInputAttributeBag()
-                    ->merge([
-                        'autocomplete' => $getAutocomplete(),
-                        'autofocus' => $isAutofocused(),
-                        'cols' => $getCols(),
-                        'disabled' => $isDisabled,
-                        'id' => $getId(),
-                        'maxlength' => (! $isConcealed) ? $getMaxLength() : null,
-                        'minlength' => (! $isConcealed) ? $getMinLength() : null,
-                        'placeholder' => $getPlaceholder(),
-                        'readonly' => $isReadOnly(),
-                        'required' => $isRequired() && (! $isConcealed),
-                        'rows' => $rows,
-                        $applyStateBindingModifiers('wire:model') => $statePath,
-                    ], escape: false)
-                    ->class([
-                        'block w-full border-none bg-transparent px-3 py-1.5 text-base text-gray-950 placeholder:text-gray-400 focus:ring-0 disabled:text-gray-500 disabled:[-webkit-text-fill-color:theme(colors.gray.500)] disabled:placeholder:[-webkit-text-fill-color:theme(colors.gray.400)] dark:text-white dark:placeholder:text-gray-500 dark:disabled:text-gray-400 dark:disabled:[-webkit-text-fill-color:theme(colors.gray.400)] dark:disabled:placeholder:[-webkit-text-fill-color:theme(colors.gray.500)] sm:text-sm sm:leading-6',
-                        'resize-none' => $shouldAutosize,
-                    ])
-                    ->style([
-                        "height: {$initialHeight}rem" => $shouldAutosize,
-                    ])
-            }}
-        ></textarea>
-        @if ($showInsideControl)
-            @include('filament-character-counter::partials.character-count-container')
-        @endif
+                {{
+                    $getExtraInputAttributeBag()
+                        ->merge([
+                            'autocomplete' => $getAutocomplete(),
+                            'autofocus' => $isAutofocused(),
+                            'cols' => $getCols(),
+                            'disabled' => $isDisabled,
+                            'id' => $getId(),
+                            'maxlength' => (! $isConcealed) ? $getMaxLength() : null,
+                            'minlength' => (! $isConcealed) ? $getMinLength() : null,
+                            'placeholder' => $getPlaceholder(),
+                            'readonly' => $isReadOnly(),
+                            'required' => $isRequired() && (! $isConcealed),
+                            'rows' => $rows,
+                            $applyStateBindingModifiers('wire:model') => $statePath,
+                        ], escape: false)
+                        ->class([
+                            'block h-full w-full border-none bg-transparent px-3 py-1.5 text-base text-gray-950 placeholder:text-gray-400 focus:ring-0 disabled:text-gray-500 disabled:[-webkit-text-fill-color:theme(colors.gray.500)] disabled:placeholder:[-webkit-text-fill-color:theme(colors.gray.400)] dark:text-white dark:placeholder:text-gray-500 dark:disabled:text-gray-400 dark:disabled:[-webkit-text-fill-color:theme(colors.gray.400)] dark:disabled:placeholder:[-webkit-text-fill-color:theme(colors.gray.500)] sm:text-sm sm:leading-6',
+                            'resize-none' => $shouldAutosize,
+                        ])
+                }}
+            ></textarea>
+        </div>
     </x-filament::input.wrapper>
     @if (!$showInsideControl)
         @include('filament-character-counter::partials.character-count-container')
